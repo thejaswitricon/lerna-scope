a07e73a543330d92c3b0cd978cdd0b63f3fc08c3
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-10-06 14:06:19 +0530

    fix(vpc): sample fix index.js

--- changelog-preset.config.js
+++ changelog-preset.config.js
@@ -9,13 +9,30 @@ module.exports = {
       // Group commits by commit type (e.g., feat, fix, chore)
       commits.forEach((commit) => {
         const commitType = commit.type || 'Other';
-        const packageName = commit.scope || 'Other'; // Default to 'Other' if no scope provided
+        let commitScope = commit.scope || 'Other'; // Default to 'Other' if no scope provided
+  
+        // If scope is 'Other', try to extract scope from the commit message or file path
+        if (commitScope === 'Other') {
+          // Extract scope from the commit message
+          const scopeMatch = commit.message.match(/\(([^)]+)\)/);
+          if (scopeMatch) {
+            commitScope = scopeMatch[1];
+          }
+          // Alternatively, you can extract scope from the file path if available
+          // const filePath = commit.files && commit.files[0];
+          // if (filePath) {
+          //   const scopeMatch = filePath.match(/\/([^/]+)\//);
+          //   if (scopeMatch) {
+          //     commitScope = scopeMatch[1];
+          //   }
+          // }
+        }
   
         // Determine the commit group based on the commit type
         const commitGroup = commitType === 'feat' ? 'Features' : 'Bug Fixes';
   
         // Add each commit message with the package name
-        commitGroups[commitGroup].push(`{${packageName}}: ${commit.message} (#${commit.hash.slice(0, 7)})`);
+        commitGroups[commitGroup].push(`${commitScope}: ${commit.message} (#${commit.hash.slice(0, 7)})`);
       });
   
       // Construct the changelog entry
