752f854cb7933c2dfbd0332ff7123577793bb60b
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-10-06 10:00:43 +0530

    fix: add st-scope.js

--- set-scope.js
+++ set-scope.js
@@ -1,21 +1,33 @@
-// set-scope.js
+const { execSync } = require('child_process');
 
-const fs = require('fs');
-const path = require('path');
+// Get the list of changed files from the input argument
+const changedFiles = process.argv[2];
 
-// Get the list of changed files
-const changedFiles = process.argv.slice(2);
+// Function to determine the scope based on the changed files
+function getScope(changedFiles) {
+  const packagesDir = 'packages'; // Change this to match your project structure
+  const changedPackages = [];
 
-// Extract the directory name from the first changed file
-const scope = path.dirname(changedFiles[0]);
+  // Split the list of changed files and check if they belong to any package
+  const files = changedFiles.split('\n').filter(Boolean);
+  for (const file of files) {
+    if (file.startsWith(packagesDir)) {
+      const packageScope = file.split('/')[1]; // Extract the package name/scope
+      if (!changedPackages.includes(packageScope)) {
+        changedPackages.push(packageScope);
+      }
+    }
+  }
 
-// Read the existing lerna.json file
-const lernaConfig = require('./lerna.json');
+  // Combine the changed package scopes as a comma-separated string
+  return changedPackages.join(',');
+}
 
-// Update the "version" field in lerna.json with the extracted scope
-lernaConfig.version = scope;
+// Set the scope using the getScope function
+const scope = getScope(changedFiles);
 
-// Write the updated lerna.json file
-fs.writeFileSync('./lerna.json', JSON.stringify(lernaConfig, null, 2));
+// Set the Lerna scope environment variable
+process.env.LERNA_SCOPE = scope;
 
-console.log(`Set scope to "${scope}" in lerna.json`);
+// Output the determined scope for debugging purposes
+console.log(`Determined Lerna scope: ${scope}`);
