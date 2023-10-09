5e218df847c7f45db777541107eee1420c0d1466
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-10-09 12:54:51 +0530

    feat: initial

--- changelog-preset.config.js
+++ changelog-preset.config.js
@@ -4,33 +4,43 @@ const path = require('path');
 module.exports = {
   // Function to generate a custom changelog entry for a package version
   generateEntry: ({ version, tag, commits }) => {
-    // Discover package names dynamically by scanning the 'packages' directory
-    const packageNames = discoverPackageNames(); // Implement this function
+    const commitGroups = {
+      'Bug Fixes': [],
+      'Features': [],
+    };
+
+    // Helper function to get the package or directory name from the file path
+    function getPackageOrDirectoryName(filePath) {
+      // Extract the directory name from the file path
+      const directoryMatch = filePath.match(/\/([^/]+)\//);
+      if (directoryMatch) {
+        return directoryMatch[1];
+      }
+      return 'Other'; // Default to 'Other' if no scope is available
+    }
+
+    // Group commits by commit type (e.g., feat, fix, chore)
+    commits.forEach((commit) => {
+      const commitType = commit.type || 'Other';
+      const commitScope = commit.scope || getPackageOrDirectoryName(commit.files && commit.files[0]);
+
+      // Determine the commit group based on the commit type
+      const commitGroup = commitType === 'feat' ? 'Features' : 'Bug Fixes';
+
+      // Add each commit message with the package name
+      commitGroups[commitGroup].push(`${commitScope}: ${commit.message} (#${commit.hash.slice(0, 7)})`);
+    });
 
     // Construct the changelog entry
     const changelogEntry = `## [${version}](${tag})\n`;
 
-    // Add a section for package names
-    if (packageNames.length > 0) {
-      const packageNamesSection = `### Packages\n${packageNames.join('\n')}`;
-      return changelogEntry + packageNamesSection + '\n';
-    }
+    // Add sections for "Bug Fixes" and "Features" if there are commits in them
+    const sections = Object.keys(commitGroups)
+      .filter((commitGroup) => commitGroups[commitGroup].length > 0)
+      .map((commitGroup) => {
+        return `### ${commitGroup}\n${commitGroups[commitGroup].join('\n')}`;
+      });
 
-    // If no package names found, return only the changelog entry
-    return changelogEntry + '\n';
+    return changelogEntry + sections.join('\n') + '\n';
   },
 };
-
-// Function to discover package names dynamically
-function discoverPackageNames() {
-  const packagesDir = path.join(__dirname, './modules/ami/*'); // Change to the path of your packages directory
-
-  if (fs.existsSync(packagesDir)) {
-    return fs.readdirSync(packagesDir).filter((entry) => {
-      const packageJsonPath = path.join(packagesDir, entry, 'package.json');
-      return fs.existsSync(packageJsonPath);
-    });
-  }
-
-  return [];
-}
