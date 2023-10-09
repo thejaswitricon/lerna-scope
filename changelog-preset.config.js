0282aae203d94bde7c7766a02fe91e5d01b9cd72
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-10-09 11:34:04 +0530

    feat: initial

--- changelog-preset.config.js
+++ changelog-preset.config.js
@@ -1,49 +1,73 @@
 module.exports = {
-    // Function to generate a custom changelog entry for a package version
-    generateEntry: ({ version, tag, commits }) => {
-      const commitGroups = {
-        'Bug Fixes': [],
-        'Features': [],
-      };
-  
-      // Group commits by commit type (e.g., feat, fix, chore)
-      commits.forEach((commit) => {
-        const commitType = commit.type || 'Other';
-        let commitScope = commit.scope; // Use provided scope if available
-  
-        // If no explicit scope is provided, try to extract scope from the file path
-        if (!commitScope) {
-          const filePath = commit.files && commit.files[0];
-          if (filePath) {
-            // Extract the directory name from the file path
-            const scopeMatch = filePath.match(/\/([^/]+)\//);
-            if (scopeMatch) {
-              commitScope = scopeMatch[1];
-            }
+  // Function to generate a custom changelog entry for a package version
+  generateEntry: ({ version, tag, commits }) => {
+    const commitGroups = {
+      'Bug Fixes': [],
+      'Features': [],
+    };
+
+    const changedPackages = new Set();
+
+    // Group commits by commit type (e.g., feat, fix, chore)
+    commits.forEach((commit) => {
+      const commitType = commit.type || 'Other';
+      let commitScope = commit.scope; // Use provided scope if available
+
+      // If no explicit scope is provided, try to extract scope from the file path
+      if (!commitScope) {
+        const filePath = commit.files && commit.files[0];
+        if (filePath) {
+          // Extract the directory name from the file path
+          const scopeMatch = filePath.match(/\/([^/]+)\//);
+          if (scopeMatch) {
+            commitScope = scopeMatch[1];
           }
         }
-  
-        // Default to 'Other' if scope is still not available
-        commitScope = commitScope || 'Other';
-  
-        // Determine the commit group based on the commit type
-        const commitGroup = commitType === 'feat' ? 'Features' : 'Bug Fixes';
-  
-        // Add each commit message with the package name
-        commitGroups[commitGroup].push(`${commitScope}: ${commit.message} (#${commit.hash.slice(0, 7)})`);
-      });
-  
-      // Construct the changelog entry
-      const changelogEntry = `## [${version}](${tag})\n`;
-  
-      // Add sections for "Bug Fixes" and "Features" if there are commits in them
-      const sections = Object.keys(commitGroups)
-        .filter((commitGroup) => commitGroups[commitGroup].length > 0)
-        .map((commitGroup) => {
-          return `### ${commitGroup}\n${commitGroups[commitGroup].join('\n')}`;
+      }
+
+      // Default to 'Other' if scope is still not available
+      commitScope = commitScope || 'Other';
+
+      // Determine the commit group based on the commit type
+      const commitGroup = commitType === 'feat' ? 'Features' : 'Bug Fixes';
+
+      // Add each commit message with the package name
+      commitGroups[commitGroup].push(`${commitScope}: ${commit.message} (#${commit.hash.slice(0, 7)})`);
+
+      // Track changed package names by looking for changes in package.json files
+      if (commit.files) {
+        commit.files.forEach((filePath) => {
+          if (filePath.endsWith('/package.json')) {
+            const packageName = filePath.match(/([^/]+)\/package.json/);
+            if (packageName) {
+              changedPackages.add(packageName[1]);
+            }
+          }
         });
-  
-      return changelogEntry + sections.join('\n') + '\n';
-    },
-  };
-  
+      }
+    });
+
+    // Construct the changelog entry
+    const changelogEntry = `## [${version}](${tag})\n`;
+
+    // Add sections for "Bug Fixes" and "Features" if there are commits in them
+    const sections = Object.keys(commitGroups)
+      .filter((commitGroup) => commitGroups[commitGroup].length > 0)
+      .map((commitGroup) => {
+        return `### ${commitGroup}\n${commitGroups[commitGroup].join('\n')}`;
+      });
+
+    // Add a section for changed package names
+    if (changedPackages.size > 0) {
+      const packageNamesSection = `### Changed Package Names\n${Array.from(changedPackages).join('\n')}`;
+      sections.push(packageNamesSection);
+    }
+
+    // Append the names of the changed package.json files
+    const changedPackageFiles = Array.from(changedPackages).map((packageName) => `${packageName}/package.json`);
+    const changedPackageFilesSection = `### Changed Package Files\n${changedPackageFiles.join('\n')}`;
+    sections.push(changedPackageFilesSection);
+
+    return changelogEntry + sections.join('\n') + '\n';
+  },
+};
