7da72ad5213a0cdeafb5514026c697fad3bf20ea
thejaswitricon <thejaswi.cy@triconinfotech.com>
2023-10-16 12:34:59 +0530

    fix: initial

--- changelog-preset.js
+++ changelog-preset.js
@@ -1,26 +1,62 @@
+const conventionalChangelog = require('conventional-changelog');
+const conventionalChangelogWriter = require('conventional-changelog-writer');
+
 module.exports = {
-    // Your changelog preset configuration here
-  
-    // Customized function to extract directory information from commit messages
-    getInfoFromCommit(commit) {
-      // Parse the commit message to extract the directory name
-      // Assuming the commit message format is "feat(package-name): your commit message"
-      const match = commit.header.match(/feat\(([^)]+)\)/);
-  
-      if (match) {
-        const directoryName = match[1];
-        return `**Directory Changed:** ${directoryName}\n\n`;
-      }
-  
-      return '';
-    },
-  
-    // Customized writer to include directory information in the changelog
-    writerOpts: {
-      transform: function(commit, context) {
-        const directoryInfo = this.getInfoFromCommit(commit);
-        return `* ${commit.type}: ${commit.subject}\n${directoryInfo}`;
-      },
-    },
-  };
-  
+  // Customized writer to include the name of the committed person in the changelog
+  writerOpts: {
+    // Add your customized options here
+    // For example, specify the format of the output, output stream, etc.
+    // See available options: https://github.com/conventional-changelog/conventional-changelog
+  },
+
+  // Customized function to retrieve the name of the committed person
+  getAuthorName(commit) {
+    return commit.committer.name;
+  },
+
+  // Customized function to add the author's name to the changelog entry
+  addAuthorName(context, commit) {
+    return new Promise((resolve, reject) => {
+      // Retrieve the author's name using the getAuthorName function
+      const authorName = this.getAuthorName(commit);
+
+      // Modify the commit object to include the author's name
+      commit.authorName = authorName;
+
+      resolve(commit);
+    });
+  },
+
+  // Customized transform function to include the author's name in the changelog entry
+  transform(commit, context) {
+    return this.addAuthorName(context, commit).then((commitWithAuthor) => {
+      const authorName = commitWithAuthor.authorName || 'Unknown Author';
+      const commitMessage = commitWithAuthor.header;
+      
+      // Include the author's name in the changelog entry
+      return conventionalChangelogWriter.transform(commitWithAuthor, context).then((output) => {
+        return `* ${commitWithAuthor.type} (${authorName}): ${commitMessage}\n${output}`;
+      });
+    });
+  },
+
+  // Function to generate changelog using conventional-changelog
+  generateChangelog() {
+    return new Promise((resolve, reject) => {
+      const stream = conventionalChangelog(this.writerOpts, this.context, this.gitRawCommitsOpts, this.parserOpts, this.generateOpts);
+      let changelog = '';
+
+      stream.on('data', (chunk) => {
+        changelog += chunk.toString();
+      });
+
+      stream.on('end', () => {
+        resolve(changelog);
+      });
+
+      stream.on('error', (err) => {
+        reject(err);
+      });
+    });
+  },
+};
